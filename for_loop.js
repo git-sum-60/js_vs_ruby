77a51a2225e321c7789d268ae716c137d21205cb
brian <riceb53@gmail.com>
2020-03-30 22:26:50 -0400

    add js solutions

---
+++ for_loop.js
@@ -0,0 +1,12 @@
+var fruits = ["mango", "pineapple", "raspberry", "cherry"];
+
+for (var i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+
+
+var i = 0
+while (i < fruits.length) {
+  console.log(fruits[i]);
+  i += 1;
+}
